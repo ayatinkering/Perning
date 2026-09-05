e70fc11b892eb5ae97f47d9cf676edc150cb6be3
guthlb <ayatirauthan@gmail.com>
2026-09-05 17:34:20 +0530

    update and delete methods added

--- Backend AI Engineering/crud-api/server.js
+++ Backend AI Engineering/crud-api/server.js
@@ -1,24 +1,19 @@
 const express=require('express')
 const app=express()
 const port=3000
-app.use(express.json()) //middleware that // parses incoming JSON request bodies into js objects
+
+app.use(express.json()) // middleware that parses incoming JSON request bodies into JS objects
 
 const tasks=[
-    {"id":1,
-    "name":"eat",
-    "done":true
-    },
-    {"id":2,
-    "name":"sleep",
-    "done":true
-    },
-    {"id":3,
-    "name":"study",
-    "done":true
-    }
+    {"id":1, "title":"eat", "done":true},
+    {"id":2, "title":"sleep", "done":true},
+    {"id":3, "title":"study", "done":true}
 ]
 
-app.get('/',(req,res)=>{        //root endpoint 
+
+
+// GET
+app.get('/',(req,res)=>{
     res.json({
         name:"Task API",
         "version":"1.0",
@@ -26,17 +21,60 @@ app.get('/',(req,res)=>{        //root endpoint
     })
 });
 
-app.get('/health',(req,res)=>{      //showing health of server
+app.get('/health',(req,res)=>{
     res.json({"status":"ok"})
 });
 
-app.get("/tasks",(req,res)=>{       //getting all tasks
+app.get("/tasks",(req,res)=>{
     res.json(tasks)
 });
 
-app.get('/tasks/:id',(req,res)=>{       //getting 1 task w PATH PARAMETERS
+app.get('/tasks/:id',(req,res)=>{ //get 1 id using path parameters
     const id=Number(req.params.id);
-    const task=tasks.find(task=>
+
+    const task=tasks.find(task =>
+        task.id===id
+    )
+
+    if(!task){ //task === undefined
+        return res.status(404).json(
+            {"error":`Task ${id} not found`}
+        )
+    }
+
+    res.json(task)
+})
+
+
+
+// POST
+app.post('/tasks',(req,res)=>{
+
+    if(!req.body.title){
+        return res.status(400).json(
+            {"error":"Title not filled"}
+        )
+    }
+
+    const newId=tasks.length + 1
+    const newTask={
+        "id":newId,
+        "title":req.body.title,
+        "done":false
+    }
+    tasks.push(newTask)
+    return res.status(201).json(
+        newTask
+    )
+});
+
+
+
+// PUT
+app.put("/tasks/:id",(req,res)=>{
+
+    const id=Number(req.params.id);
+    const task=tasks.find(task =>
         task.id===id
     )
 
@@ -45,27 +83,53 @@ app.get('/tasks/:id',(req,res)=>{       //getting 1 task w PATH PARAMETERS
             {"error":`Task ${id} not found`}
         )
     }
-    res.json(task)
-})
 
-app.post('/tasks',(req,res)=>{
-    if(!req.body.title){
-        res.status(400).json(
-            {"error":"Title not filled"}
-        )
+    // Nothing was supplied
+    if(req.body.title === undefined && req.body.done === undefined){
+        return res.status(400).json({
+            error:"Request body must contain title or done"
+        });
     }
-    const newId=tasks.length + 1
-    const newTask={
-        "id":newId,
-        "name":req.body.title,
-        "done":false
-    }
-    tasks.push(newTask)
 
-    return res.status(201).json(
-        newTask
-    )
+    if(req.body.title !== undefined){ // title supplied
+        if(typeof req.body.title === "string")
+            task.title=req.body.title
+        else
+            return res.status(400).json({
+                "error":"title must be string"
+            })
+    }
+
+    if(req.body.done !== undefined){ // done true/false supplied
+        if(typeof req.body.done === "boolean")
+            task.done=req.body.done
+        else
+            return res.status(400).json({
+                "error":"done status must be boolean"
+            })
+    }
+
+    res.json(task);
 });
+
+
+
+// DELETE
+app.delete('/tasks/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    const index=tasks.findIndex(task =>
+        task.id===id
+    )
+
+    if(index===-1){
+        return res.status(404).json({
+            "error":`Task ${id} not found`
+        })
+    }
+
+    tasks.splice(index,1)
+    return res.status(204).send(); //204 means sucessfuly deleted, send returns NOTHING
+})
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)
